192feb6f66182f6668bd915eab1ca3db3649e9eb
Felipe Silveira <felipelarson@msn.com>
2022-05-30 18:02:34 +0100

    update: urls.py

--- mercado/urls.py
+++ mercado/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
+from address.views import AddressViewSet
 from category.views import CategoryViewSet
 
 router = routers.DefaultRouter()
 
+router.register(r"addresses", AddressViewSet, basename="addresses")
 router.register(r"categories", CategoryViewSet, basename="categories")
 
 urlpatterns = [
