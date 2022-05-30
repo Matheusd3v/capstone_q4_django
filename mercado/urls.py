c5f7b06a83128a2bac784837e11d371ca839d5a3
Matheus <matheus.g.almeida21@gmail.com>
2022-05-30 14:12:53 -0300

    feat: add route in mercado urls

--- mercado/urls.py
+++ mercado/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from rest_framework import routers
 
 from category.views import CategoryViewSet
+from brand.views import BrandViewSet
 
 router = routers.DefaultRouter()
 
 router.register(r"categories", CategoryViewSet, basename="categories")
+router.register(r"brands", BrandViewSet, basename="brands")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
