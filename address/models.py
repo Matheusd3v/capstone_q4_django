cfc32ece80f38181e6aa8d3992ce43214223c720
Felipe Silveira <felipelarson@msn.com>
2022-05-30 17:57:19 +0100

    update: models.py

--- address/models.py
+++ address/models.py
@@ -1,3 +1,18 @@
+from uuid import uuid4
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    class Meta:
+        verbose_name_plural = "addresses"
+
+    address_uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    street = models.CharField(max_length=255)
+    number = models.IntegerField()
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "%s-%s (%s)" % (self.city, self.state, self.country)
