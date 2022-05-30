e905e7674d8ad7d338cd31d3da1e62c9bfd0c8e4
Felipe Silveira <felipelarson@msn.com>
2022-05-30 18:01:31 +0100

    update: settings.py

--- mercado/settings.py
+++ mercado/settings.py
@@ -45,9 +45,7 @@ THIRD_PARTY_APPS = [
     "rest_framework.authtoken",
 ]
 
-MY_APPS = [
-    "category",
-]
+MY_APPS = ["category", "address"]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + MY_APPS
 
