410dfb00a7f871d346bd7566726303c6098d6ede
smilly <smilly.3d@gmail.com>
2022-05-30 17:56:32 -0300

    feat: model has done

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
+from uuid import uuid4
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    user_uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    address = models.ForeignKey(
+        "address.Address", on_delete=models.RESTRICT, related_name="users", null=True
+    )
+
+    def __str__(self):
+        return "%s - %s" % (self.username, self.address)
