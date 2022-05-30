0508b9024ea04536f8a68a6e518a20872f258e78
matheusburey <matheushburei.99@gmail.com>
2022-05-30 13:55:50 -0300

    feat(products): created products app

---
+++ products/models.py
@@ -0,0 +1,22 @@
+from uuid import uuid4
+from django.db import models
+
+
+class Products(models.Model):
+    class Meta:
+        verbose_name_plural = "products"
+
+    products_uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=60)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+
+    brand = models.ForeignKey(
+        "brand.Brand", on_delete=models.PROTECT, related_name="products"
+    )
+    category = models.ForeignKey(
+        "category.Category", on_delete=models.PROTECT, related_name="products"
+    )
+
+    def __str__(self):
+        return "%s (%s)" % (self.name, self.brand)
