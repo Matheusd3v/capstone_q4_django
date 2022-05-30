ad4853e933cb65b9a4501d25a130b8f219382edd
Felipe Silveira <felipelarson@msn.com>
2022-05-30 21:08:51 +0100

    update: settings.py

--- mercado/settings.py
+++ mercado/settings.py
@@ -44,13 +44,10 @@ DJANGO_APPS = [
 THIRD_PARTY_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
+    "drf_spectacular",
 ]
 
-MY_APPS = [
-    "category",
-    "brand",
-    "products"
-]
+MY_APPS = ["category", "brand", "products"]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + MY_APPS
 
@@ -141,9 +138,18 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
-    ]
+    ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+SPECTACULAR_SETTINGS = {
+    "TITLE": "API Mercadinho do Zé",
+    "DESCRIPTION": "API com os endpoints e documentação do mercado do Zé",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    # OTHER SETTINGS
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
