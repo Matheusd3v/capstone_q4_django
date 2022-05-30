b7a6848693c4317b6013229c9e14ca976b2a7f16
smilly <smilly.3d@gmail.com>
2022-05-30 17:56:45 -0300

    feat: admin has done

--- purchase/admin.py
+++ purchase/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from purchase.models import Purchase, ItensPurchase
+
+
+class ItensPurchaseInline(admin.TabularInline):
+    model = ItensPurchase
+
+
+class PurchaseAdmin(admin.ModelAdmin):
+    inlines = [ItensPurchaseInline]
+
+
+admin.site.register(Purchase, PurchaseAdmin)
