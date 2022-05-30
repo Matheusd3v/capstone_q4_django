2a09c79acdcb9d50104e2132ef93ce5be82fa620
Felipe Silveira <felipelarson@msn.com>
2022-05-30 18:40:44 +0100

    update: views.py

--- address/views.py
+++ address/views.py
@@ -7,3 +7,10 @@ from address.serializers import AddressSerializer
 class AddressViewSet(ModelViewSet):
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
+
+    def get_queryset(self):
+        usuario = self.request.user
+
+        if usuario.groups.filter(name="Administradores"):
+            return Address.objects.all()
+        return Address.objects.filter(usuario=usuario)
