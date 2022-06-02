0087d3f314081973e9df52f7957d55f34fc29bab
smilly <smilly.3d@gmail.com>
2022-06-02 19:12:56 -0300

    feature: implement get and get/:<uuid> filter per admin

--- user/views.py
+++ user/views.py
@@ -1,14 +1,27 @@
 from rest_framework.viewsets import ModelViewSet
 from user.models import User
 from user.permissions import CreateSuperuser
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.views import Response
 
 from user.serializers import UsersSerializer
 
 
 class UserViewSet(ModelViewSet):
-    http_method_names = ["post", "head"]
+    http_method_names = ["get", "post", "head"]
     permission_classes = [CreateSuperuser]
 
     queryset = User.objects.all()
     serializer_class = UsersSerializer
+
+    def get_queryset(self):
+
+        if self.request.user.is_anonymous:
+            raise AuthenticationFailed("Missing authorization token.")
+
+        user = self.request.user
+
+        if user.is_superuser:
+            return User.objects.all()
+
+        return User.objects.filter(user_uuid=user.user_uuid)
