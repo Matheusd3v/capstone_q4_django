95fe60b974e48e818fa526b9c56f1de4d7f3b81c
smilly <smilly.3d@gmail.com>
2022-06-02 17:46:00 -0300

    remove: refresh token view

--- mercado/urls.py
+++ mercado/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
-    TokenRefreshView,
 )
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -31,7 +30,6 @@ router.register(r"user", UserViewSet, basename="user")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("user/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("user/login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/swagger/",
