ce5eabdb65dbcb8cfcf9577f11937df0c369186e
smilly <smilly.3d@gmail.com>
2022-06-02 21:45:56 -0300

    fix: global permission

--- mercado/permissions.py
+++ mercado/permissions.py
@@ -7,17 +7,6 @@ from user.models import User
 SAFE_METHODS = ["GET", "HEAD", "OPTIONS"]
 
 
-class CreateSuperuser(BasePermission):
-    def has_permission(self, request: Request, _):
-        user: User = request.user
-        is_superuser: bool = request.data.get("is_superuser", False)
-
-        if is_superuser and (not user.is_authenticated or not user.is_superuser):
-            raise NotAuthenticated("Only admin can create a superuser.")
-
-        return True
-
-
 class ReadOnly(BasePermission):
     """
     The request is authenticated as a user, or is a read-only request.
